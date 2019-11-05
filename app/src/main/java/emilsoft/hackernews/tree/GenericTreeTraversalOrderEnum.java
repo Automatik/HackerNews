b36bbee90e9b9834f8e45e60d8462387c937d92d
e.osterhed <e.osterhed@campus.unimib.it>
2019-11-05 10:30:35 +0100

    Add GenericTree to store and retrieve comments

---
+++ app/src/main/java/emilsoft/hackernews/tree/GenericTreeTraversalOrderEnum.java
@@ -0,0 +1,11 @@
+/*
+ Copyright 2010 Vivin Suresh Paliath
+ Distributed under the BSD License
+*/
+
+package emilsoft.hackernews.tree;
+
+public enum GenericTreeTraversalOrderEnum {
+    PRE_ORDER,
+    POST_ORDER
+}
